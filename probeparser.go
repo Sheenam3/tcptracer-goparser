5d6b7128ea8fbddf5e385710e9c43b0f9638cce0
root <root@dad.cluster.local>
2020-03-31 18:15:08 +0800

    pid filter test

--- probeparser.go
+++ probeparser.go
@@ -1,247 +1,275 @@
 package main
 
 import (
-        "bufio"
-        //"encoding/json"
-        "fmt"
-        ps "github.com/mitchellh/go-ps"
-        "log"
-        "os"
-        "os/exec"
-        //"runtime"
-        "strconv"
-        "strings"
-        "time"
+"bufio"
+//"encoding/json"
+//"fmt"
+"log"
+//"os"
+"os/exec"
+//"runtime"
+"strconv"
+"strings"
+"time"
 )
 
 type Log struct {
-        log   string
-        pid   int64
-        time  float64
-        probe string
+	Fulllog   string
+	Pid   int64
+	Time  float64
+	Probe string
 }
 
-//var logging []Log
-//var tcplog []Log
+/*type Monitor struct {
+	Alloc,
+	TotalAlloc,
+	Sys,
+	Mallocs,
+	Frees,
+	LiveObjects,
+	PauseTotalNs uint64
+	NumGC        uint32
+	NumGoroutine int
+}
+*/
 
+//var IsTracerDoneSig = make(chan bool, 1)
+//var IsTCPTracerDoneSig = make(chan bool, 1)
 const (
-        timestamp int = 0
+	timestamp int = 0
 )
 
+/*func NewMonitor(duration int) {
+	var m Monitor
+	var rtm runtime.MemStats
+	var interval = time.Duration(duration) * time.Second
+	for {
+		<-time.After(interval)
 
+		// Read full mem stats
+		runtime.ReadMemStats(&rtm)
 
-func runTCP(tool string, logchannel chan Log) {
+		// Number of goroutines
+		m.NumGoroutine = runtime.NumGoroutine()
 
-        //quit := make(chan bool)
-        fmt.Println("checkpoint 1")
-        if tool == "tcptracer" {
-                cmd := exec.Command("./tcptracer", "-t")
-                cmd.Dir = "/usr/share/bcc/tools"
-                stdout, err := cmd.StdoutPipe()
-                if err != nil {
-                        log.Fatal(err)
-                }
-                cmd.Start()
-                fmt.Println("checkpoint 2")
-                probePid := cmd.Process.Pid
-                log.Printf("pid: %d", cmd.Process.Pid)
-                probeName, err := ps.FindProcess(probePid)
-                if err != nil {
-                        fmt.Println("Error : ", err)
-                        os.Exit(-1)
-                }
-                fmt.Println("checkpoint 3")
-                log.Printf("Probe Name: %v", probeName.Executable())
-                buf := bufio.NewReader(stdout)
-                //num := 1
-                for {
-                        fmt.Println("checkpoint 4")
-                        line, _, _ := buf.ReadLine()
-                        fmt.Println("checkpoint 5")
-                        parsedLine := strings.Fields(string(line))
-                        fmt.Println("checkpoint 6")
-                        println("TCP TRACER", parsedLine[0])
-                        if parsedLine[0] != "Tracing" {
-                                if parsedLine[0] != "TIME(s)" {
-                                        ppid, err := strconv.ParseInt(parsedLine[2], 10, 64)
-                                        if err != nil {
-                                                println("Tcptracer PID Error")
-                                        }
-                                        fmt.Printf("no idea")
-                                        timest, err := strconv.ParseFloat(parsedLine[timestamp], 64)
-                                        if err != nil {
-                                                println(" Tcptracer Timestamp Error")
-                                        }
+		// Misc memory stats
+		m.Alloc = rtm.Alloc
+		m.TotalAlloc = rtm.TotalAlloc
+		m.Sys = rtm.Sys
+		m.Mallocs = rtm.Mallocs
+		m.Frees = rtm.Frees
 
-                                        pn := probeName.Executable()
-                                        n := Log{log: string(line), pid: ppid, time: timest, probe: pn}
+		// Live objects = Mallocs - Frees
+		m.LiveObjects = m.Mallocs - m.Frees
 
-                                        logchannel <- n
-                                        //tcplog = append(tcplog, n)
+		// GC Stats
+		m.PauseTotalNs = rtm.PauseTotalNs
+		m.NumGC = rtm.NumGC
 
-                                }
-                        }
-
-                        /*if num > 10 {
-                                for i := 0; i < 9; i++ {
-                                        fmt.Printf("Struct %d  includes: %v\n", i, tcplog[i])
-                                        fmt.Printf("Output %d: %v\n PID:%v \t| TimeStamp:%v \t | ProbeName:%v \n", i, tcplog[i].log, tcplog[i].pid, tcplog[i].time, tcplog[i].probe)
-                                }
-                                //quit <- true
-                        }
-                        num += 1
-*/
-                }
-        }
-/*
-        if tool == "tcpconnect" {
-                cmd := exec.Command("./tcpconnect", "-t")
-                cmd.Dir = "/usr/share/bcc/tools"
-                stdout, err := cmd.StdoutPipe()
-                if err != nil {
-                        log.Fatal(err)
-                }
-                cmd.Start()
-                probePid := cmd.Process.Pid
-                log.Printf("pid: %d", cmd.Process.Pid)
-                probeName, err := ps.FindProcess(probePid)
-                if err != nil {
-                        fmt.Println("Error : ", err)
-                        os.Exit(-1)
-                }
-                log.Printf("Probe Name: %v", probeName.Executable())
-
-                buf := bufio.NewReader(stdout)
-                num := 1
-
-                for {
-                        line, _, _ := buf.ReadLine()
-                        parsedLine := strings.Fields(string(line))
-                        //println(parsedLine[0])
-                        if parsedLine[0] != "TIME(s)" {
-                                ppid, err := strconv.ParseInt(parsedLine[1], 10, 64)
-                                if err != nil {
-                                        println("TCPConnect PID Error")
-                                }
-                                timest, err := strconv.ParseFloat(parsedLine[timestamp], 64)
-                                if err != nil {
-                                        println(" TCPConnect Timestamp Error")
-                                }
-
-                                pn := probeName.Executable()
-                                n := Log{log: string(line), pid: ppid, time: timest, probe: pn}
-                                logging = append(logging, n)
-
-                        }
-
-                        if num > 10 {
-                                for i := 0; i < 9; i++ {
-                                        fmt.Printf("Struct %d  includes: %v\n", i, logging[i])
-                                        fmt.Printf("Output %d: %v\n PID:%v \t| TimeStamp:%v \t | ProbeName:%v \n", i, logging[i].log, logging[i].pid, logging[i].time, logging[i].probe)
-                                }
-                                //quit <- true
-                        }
-                        num += 1
-
-                }
-        }*/
-
-}
-/*
-type Monitor struct {
-        Alloc,
-        TotalAlloc,
-        Sys,
-        Mallocs,
-        Frees,
-        LiveObjects,
-        PauseTotalNs uint64
-
-        NumGC        uint32
-        NumGoroutine int
-}
-
-func NewMonitor(duration int) {
-        var m Monitor
-        var rtm runtime.MemStats
-        var interval = time.Duration(duration) * time.Second
-        for {
-                <-time.After(interval)
-
-                // Read full mem stats
-                runtime.ReadMemStats(&rtm)
-
-                // Number of goroutines
-                m.NumGoroutine = runtime.NumGoroutine()
-
-                // Misc memory stats
-                m.Alloc = rtm.Alloc
-                m.TotalAlloc = rtm.TotalAlloc
-                m.Sys = rtm.Sys
-                m.Mallocs = rtm.Mallocs
-                m.Frees = rtm.Frees
-
-                // Live objects = Mallocs - Frees
-                m.LiveObjects = m.Mallocs - m.Frees
-
-                // GC Stats
-                m.PauseTotalNs = rtm.PauseTotalNs
-                m.NumGC = rtm.NumGC
-
-                // Just encode to json and print
-                b, _ := json.Marshal(m)
-                fmt.Println(string(b))
-        }
-}
-func printStats(mem runtime.MemStats) {
-
-        runtime.ReadMemStats(&mem)
-
-        fmt.Println("mem.Alloc:", mem.Alloc)
-
-        fmt.Println("mem.TotalAlloc:", mem.TotalAlloc)
-
-        fmt.Println("mem.HeapAlloc:", mem.HeapAlloc)
-
-        fmt.Println("mem.NumGC:", mem.NumGC)
-
-        fmt.Println("-----")
-
+		// Just encode to json and print
+		b, _ := json.Marshal(m)
+		fmt.Println(string(b))
+	}
 }*/
 
-func main() {
+func RunTcptracer(tool string, logtcptracer chan Log, pid [][]string) {
 
-        logchannel := make(chan Log,1)
+		var pd string
+		for j := range pid {
 
-        go runTCP("tcptracer", logchannel )
+			for k := range pid[j] {
+			        pd = pid[j][k]
+			println("pid in array:",pd)
 
-        data := <-logchannel
+			}
+		}
+		cmd := exec.Command("./tcptracer", "-t", "-p" + pd)
+		cmd.Dir = "/usr/share/bcc/tools"
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+		cmd.Start()
+		buf := bufio.NewReader(stdout)
+		num := 1
 
-        fmt.Sprintf("%-8s %-6s %-8s %-10s %-5s %-15s %-15s %-5s %-5s\n", "TIME(s)", "T", "PID", "COMM", "IP", "SADDR", "DADDR", "SPORT","DPORT")
+		for {
 
-        var logstring string
+			line, _, _ := buf.ReadLine()
+			parsedLine := strings.Fields(string(line))
+			//println("TCP TRACER", parsedLine[0])
+			if parsedLine[0] != "Tracing" {
+				if parsedLine[0] != "TIME(s)" {
+					ppid, err := strconv.ParseInt(parsedLine[2], 10, 64)
+					if err != nil {
+						println("Tcptracer PID Error")
+					}
+					timest, err := strconv.ParseFloat(parsedLine[timestamp], 64)
+					if err != nil {
+						println(" Tcptracer Timestamp Error")
+					}
+					n := Log{Fulllog: string(line), Pid: ppid, Time: timest, Probe: tool}
+					logtcptracer <- n
+					if num > 1000 {
+                                                close(logtcptracer)
+                                                log.Println("Tracer has been Stopped")
+//                                                IsTCPTracerDoneSig <- true
 
-        logstring = fmt.Sprintf("%-8s %-6s %-8d %-5s IPv%-2d %-15s %-15s %-5d %-5d \n ", "PID: %d \n", "ProbeName: %s \n", "TimeStamp: %v", data.log, data.pid, data.probe, data.time)
+                        	        }
+                            	num++
+				//Tcplog = append(Tcplog, n)
+				}
+			}
+		}
+}
 
-        fmt.Printf(logstring)
 
-//      go runTCP("tcpconnect")
+
+
+func RunTcpconnect(tool string, logtcpconnect chan Log) {
+
+
+	
+/*			if num > 10 {
+				for i := 0; i < 9; i++ {
+					return Tcplog
+					/*fmt.Printf("Struct %d  includes: %v\n", i, tcplog[i])
+					fmt.Printf("Output %d: %v\n PID:%v \t| TimeStamp:%v \t | ProbeName:%v \n", i, tcplog[i].log, tcplog[i].pid, tcplog[i].time, tcplog[i].probe)
+				}
+				//quit <- true
+			}
+			num += 1
+
+		}
+	}*/
+
+	//if tool == "tcpconnect" {
+		cmd := exec.Command("./tcpconnect", "-t")
+		cmd.Dir = "/usr/share/bcc/tools"
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+		cmd.Start()
+		buf := bufio.NewReader(stdout)
+		num := 1
+
+		for {
+			line, _, _ := buf.ReadLine()
+			parsedLine := strings.Fields(string(line))
+			//println(parsedLine[0])
+			if parsedLine[0] != "TIME(s)" {
+				ppid, err := strconv.ParseInt(parsedLine[1], 10, 64)
+				if err != nil {
+					println("TCPConnect PID Error")
+				}
+				timest, err := strconv.ParseFloat(parsedLine[timestamp], 64)
+				if err != nil {
+					println(" TCPConnect Timestamp Error")
+				}
+
+
+				n := Log{Fulllog: string(line), Pid: ppid, Time: timest, Probe: tool}
+				logtcpconnect <- n
+				if num > 300 {
+						close(logtcpconnect)
+						log.Println("Tracerconnect has been Stopped")					
+//						IsTracerDoneSig <- true
+						
+				}
+				num++
+				//Logging = append(Logging, n)
+
+			}
 /*
-        //var mem runtime.MemStats
-        /fmt.Println("HAHAHAHAHAHAHHAAHHA")
-        time.Sleep(10 * time.Second)
-        for {
-                time.Sleep(10 * time.Second)
-                fmt.Println("INSIDE HAHAHAHAHAHAHHAAHHA")
-                //printStats(mem)
-                //go NewMonitor(10)
+			if num > 10 {
+				for i := 0; i < 9; i++ {
+					return Logging
+					/*fmt.Printf("Struct %d  includes: %v\n", i, Logging[i])
+					fmt.Printf("Output %d: %v\n PID:%v \t| TimeStamp:%v \t | ProbeName:%v \n", i, Logging[i].Log, Logging[i].Pid, Logging[i].Time, Logging[i].Probe)
+				}
 
-        }*/
+			}
+			num += 1*/
+
+		}
+	//}
+	
+}
 
 
-        /*for i := 0; i < 19; i++ {
-                fmt.Printf("Struct %d  includes: %v\n", i, logging[i])
-                fmt.Printf("Output %d: %v\n PID:%v \t| TimeStamp:%v \t | ProbeName:%v \n", i, logging[i].log, logging[i].pid, logging[i].time, logging[i].probe)
-        }*/
 
+func getPID() [][]string{
+
+
+		var matrix [][]string
+		var row []string
+
+		
+		cmd := exec.Command("./tcptracer", "-t")
+		cmd.Dir = "/usr/share/bcc/tools"
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+		cmd.Start()
+		buf := bufio.NewReader(stdout)
+		num := 1
+
+		for {
+
+			line, _, _ := buf.ReadLine()
+			parsedLine := strings.Fields(string(line))
+			//println("TCP TRACER", parsedLine[0])
+			if parsedLine[0] != "Tracing" {
+				if parsedLine[0] != "TIME(s)" {
+					ppid, err := strconv.ParseInt(parsedLine[2], 10, 64)
+					if err != nil {
+						println("Tcptracer PID Error")
+					}
+					s := strconv.FormatInt(ppid, 10)
+					row = append(row,s)
+					matrix = append(matrix, row) 
+					//timest, err := strconv.ParseFloat(parsedLine[timestamp], 64)
+					//if err != nil {
+					//	println(" Tcptracer Timestamp Error")
+					//}
+				//	n := Log{Fulllog: string(line), Pid: ppid, Time: timest, Probe: tool}
+				println(matrix)
+				}
+			}
+				for  num > 5 {
+
+					return matrix					
+				}
+
+		}
+
+}
+
+
+func main() {
+	//go RunTCP("tcptracer")
+	logtcptracer :=  make(chan Log, 1)
+//	logtcpconnect := make(chan Log, 1)
+
+	var pid [][]string
+	pid = getPID()	
+	go RunTcptracer("tcptracer", logtcptracer,pid)
+	for val := range logtcptracer {
+	log.Printf("%v Probe: %s, Pid: %d", val.Fulllog, val.Probe, val.Pid)
+
+	}
+	/*
+	go RunProbe("tcpconnect", logtcpconnect)
+	for val := range logtcpconnect {
+	log.Printf("%v Probe: %s, Pid: %d", val.Fulllog, val.Probe, val.Pid)
+
+	}*/
+
+	for 
+	{
+
+		time.Sleep(10 * time.Second)
+	}
 }
